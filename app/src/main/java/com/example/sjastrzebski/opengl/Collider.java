b7bb2e9b5291dcd6cd5d7a370d7006dad4aa3f3c
mteblowski <maxer360@o2.pl>
2018-06-20 17:40:31 +0200

    Znikanie (Teoria magika)

--- app/src/main/java/com/example/sjastrzebski/opengl/Collider.java
+++ app/src/main/java/com/example/sjastrzebski/opengl/Collider.java
@@ -24,7 +24,66 @@ public class Collider {
         obstaclesPos = obstacles;
         score = time;
         onUpdate();
+        float x=obstacles.get(x);
+        float y=obstacles.get(y);
+        float z=obstacles.get(z);
+        float x1=ship.(x);
+        float y1=ship.(y);
 
-      //  if(obstacles.equals(obstacles.get(y)))
+        if(obstacles.equals( x && y == x1 && y1 )){
+
+
+        }
+     /*   bool Aabb::Collides(const Aabb & box) const {
+            if ( x && y == x1 && y1) {
+                return false;
+            }
+            return true;
+        }
+        */
+     /*
+        bool check_collision( SDL_Rect A, SDL_Rect B )
+        {
+
+            int leftA, leftB;
+            int rightA, rightB;
+            int topA, topB;
+            int bottomA, bottomB;
+
+
+            leftA = A.x;
+            rightA = A.x + A.w;
+            topA = A.y;
+            bottomA = A.y + A.h;
+
+
+            leftB = B.x;
+            rightB = B.x + B.w;
+            topB = B.y;
+            bottomB = B.y + B.h;
+
+            if( bottomA <= topB )
+            {
+                return false;
+            }
+
+            if( topA >= bottomB )
+            {
+                return false;
+            }
+
+            if( rightA <= leftB )
+            {
+                return false;
+            }
+
+            if( leftA >= rightB )
+            {
+                return false;
+            }
+
+            return true;
+        }
+        */
     }
 }
